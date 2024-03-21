68aac68ea26ba7400a92bf477712d127748c2544
yannstkk <yannyanop@gmail.com>
2024-03-21 19:32:17 +0100

    six

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const cheerio = require('cheerio');
 const app = express();
 const port = process.env.port || 3004;
 
+// Middleware pour gérer les en-têtes CORS
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,PATCH,POST,DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 app.use(express.json());
 
 // Route pour le scraping des navires sur la page principale
